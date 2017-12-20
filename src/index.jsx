048010bbeb36230976c1bc175f6fe766c51166b7
Ximsn <iman@heidari.ca>
2017-12-20 01:37:41 -0800

    add and setup redux, saga and axios

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import reducer from './reducers';
+import sagaControler from './saga';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const store = createStore(reducer);
+const sagaMiddleware = createSagaMiddleware(sagaControler);
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(sagaControler);
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/saga/index.js
@@ -0,0 +1,20 @@
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
+
+function* fetchUser(action) {
+  try {
+    const user = { userId: 1, username: 'imsn' };// yield call(Api.fetchUser, action.payload.userId);
+    yield put({ type: 'USER_FETCH_SUCCEEDED', user });
+  } catch (e) {
+    yield put({ type: 'USER_FETCH_FAILED', message: e.message });
+  }
+}
+
+/*
+  Starts fetchUser on each dispatched `USER_FETCH_REQUESTED` action.
+  Allows concurrent fetches of user.
+*/
+function* sagaControler() {
+  yield takeEvery('USER_FETCH_REQUESTED', fetchUser);
+}
+
+export default sagaControler;
