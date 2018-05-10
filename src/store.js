6a40c5f781d481981cef34f9e7847be1f4f0a804
Iman Heidari <iman.heidari@gmail.com>
2018-05-09 22:45:24 -0700

    fix Redux DevTools Extension problem

--- src/store.js
+++ src/store.js
@@ -23,9 +23,7 @@ function configureStoreDev(initialState) {
     //reduxImmutableStateInvariant(),
     sagaMiddleware
   ];
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__();
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     reducer,
     initialState,
